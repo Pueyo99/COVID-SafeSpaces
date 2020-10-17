28b2c1f7224d456ecd845a4097689a9d1c2b1e3e
MiquelTorrecilla <Martipol1!>
2020-10-17 18:13:40 +0200

    Añadido actividad prueba, es el menu

---
+++ App/app/src/main/java/com/example/covidsafespaces/Prueba.java
@@ -0,0 +1,44 @@
+package com.example.covidsafespaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Prueba extends AppCompatActivity {
+
+    Button entrar;
+    Button registrar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba);
+
+        TextView usuario = findViewById(R.id.usertext);
+        TextView password = findViewById(R.id.password);
+        registrar = (Button)findViewById(R.id.registrar);
+        entrar = (Button)findViewById(R.id.entrar);
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent entrar = new Intent(Prueba.this, Main.class);
+                startActivity(entrar);
+            }
+        });
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registrar = new Intent(Prueba.this, Main.class);
+                startActivity(registrar);
+            }
+        });
+    }
+}
