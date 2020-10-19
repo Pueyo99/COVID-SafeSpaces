ff6d1a5ba0a0e5016dfad623b8ef932641ddd90e
Pueyo99 <55914406+Pueyo99@users.noreply.github.com>
2020-10-19 17:38:34 +0200

    Delete process_image2.py

--- semantic-segmentation-pytorch/process_image2.py
+++
@@ -1,8 +0,0 @@
-import os
-import matplotlib.pyplot as plt
-
-image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'example.JPG')
-img = plt.imread(image_path)
-fig, ax = plt.subplots(1, figsize=(15, 15))
-ax.imshow(img)
-plt.show()
