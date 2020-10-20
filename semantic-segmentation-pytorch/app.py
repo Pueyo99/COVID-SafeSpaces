01c109c697ee476f80d126e689686c24a37ddc20
paesav <bruno@paechatbot.com>
2020-10-20 20:46:26 +0200

    Update resize function

--- semantic-segmentation-pytorch/app.py
+++ semantic-segmentation-pytorch/app.py
@@ -35,4 +35,4 @@ def read_image():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5902)
--- semantic-segmentation-pytorch/pre_process_image.py
+++ semantic-segmentation-pytorch/pre_process_image.py
@@ -1,12 +1,14 @@
 from PIL import Image
 
 def resize_images(test_path):
-    for p in test_path:
 
-        p = p["fpath_img"]
-        img = Image.open(p)
-        width, height = img.size
+	for p in test_path:
+		p = p["fpath_img"]
+		img = Image.open(p)
+		width, height = img.size
 
-        if width > 1000 or height > 1000:
-            img = img.resize((round(width/4),round(height/4)),Image.LANCZOS)
-            img.save(p)
+	while width > 1000 or height > 1000:
+		width /= 2
+		height /= 2
+		img = img.resize((round(width/2),round(height/2)),Image.LANCZOS)
+		img.save(p)
