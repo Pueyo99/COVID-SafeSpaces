05e20b7e91ae2fb9c02851c31f40f7af20a3b14f
clemens7912 <clemens7912@gmail.com>
2020-10-20 10:44:58 +0200

    Asks for gps activation when it is disabled

--- App/app/src/main/java/com/example/covidsafespaces/Main.java
+++ App/app/src/main/java/com/example/covidsafespaces/Main.java
@@ -343,37 +343,6 @@ public class Main extends AppCompatActivity implements Listener {
         alertDialog.create().show();
     }
 
-    /*public void showSettingsAlert(){
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Main.this);
-
-        // Setting Dialog Title
-        alertDialog.setTitle("GPS is settings");
-
-        // Setting Dialog Message
-        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
-
-        // On pressing the Settings button.
-        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog,int which) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                Main.this.startActivity(intent);
-            }
-        });
-
-        // On pressing the cancel button
-        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-                finish();
-            }
-        });
-
-        // Showing Alert Message
-        alertDialog.show();
-    }
-
-     */
-
     public boolean isLocationEnabled()
     {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
