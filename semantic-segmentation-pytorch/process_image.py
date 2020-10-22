daea2b9d08116dbc088edffe938fdf576f8ce6e3
paesav <bruno@paechatbot.com>
2020-10-22 13:15:15 +0200

    JSON implementation and IP change

--- semantic-segmentation-pytorch/process_image.py
+++ semantic-segmentation-pytorch/process_image.py
@@ -1,24 +1,27 @@
 from PIL import Image
+import json
 
 def calculate_window(img_name):
-    img = Image.open(img_name)
+	img = Image.open(img_name)
+	width, height = img.size
 
-    width, height = img.size
+	img_original = img.crop((0,0,width/2,height))
+	img_crop = img.crop((width/2,0,width,height))
 
-    img_original = img.crop((0,0,width/2,height))
-    img_crop = img.crop((width/2,0,width,height))
+	colors = img_crop.getcolors()
 
-    colors = img_crop.getcolors()
-    #print(colors)
 
-    for c in colors:
-        if c[1] == (230,230,230):
-            window_pixels = c[0]
-            total_pixels = (width*height)/2
-            percentage = (window_pixels/total_pixels)*100
-            print(f"Percentage of windows = {percentage}")
-        else:
-            print("No windows found")
-
-    img_original.show()
-    img_crop.show()
+	for c in colors:
+		if c[1] == (230,230,230):
+			window_pixels = c[0]
+			total_pixels = (width*height)/2
+			percentage = (window_pixels/total_pixels)*100
+		else:
+			percentage = 0
+		
+	img_name.replace('result_images', 'json_results')
+	with open(img_name.replace('.png','.json'), "w+") as json_file:
+		json.dump({ "Window percentage" : percentage }, json_file)
+	
+	img_original.show()
+	img_crop.show()
