2d5fbb4bdffe5c30f03f2d328f5b64243cc42171
clemens7912 <clemens7912@gmail.com>
2020-10-22 16:40:15 +0200

    Api login supported

--- api/app.py
+++ api/app.py
@@ -4,6 +4,7 @@ import base64
 from io import BytesIO
 import datetime
 import time
+import database
 
 SQR_M_PERSON = 4.0
 
@@ -21,6 +22,17 @@ def max_cap(room_dim):
         )
 
 
+@app.route('/<string:username>', methods = ['GET'])
+def login(username):
+	if (username != "favicon.ico"):
+		db = database.Database()
+		password = db.selectUser(username)
+		db.close()
+		return jsonify({'password':password})
+	else:
+		return jsonify({'error':'favicon'})
+
+
 @app.route('/image', methods = ['POST'])
 def read_image():
 	json = request.json
@@ -32,6 +44,16 @@ def read_image():
 	image.save("images/"+filename+".jpeg")
 	return jsonify({'prueba':'Imagen guardada'})
 
+
+@app.route('/register', methods = ['POST'])
+def register():
+	json = request.json
+	username = json['username']
+	password = json['password']
+	db = database.Database()
+	db.registerUser(username, password)
+	db.close()
+	return jsonify({'register':'successful register'})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
