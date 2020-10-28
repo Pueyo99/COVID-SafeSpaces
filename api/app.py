963b242d2aa30b535d9e9d7ac1737d50625242cd
Jorge <pueyo99@gmail.com>
2020-10-28 09:58:44 +0100

    Corrected port

--- api/app.py
+++ api/app.py
@@ -65,4 +65,4 @@ def register():
 """
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', ssl_context = context)
+    app.run(debug=True, host='0.0.0.0', port=8999, ssl_context = context)
