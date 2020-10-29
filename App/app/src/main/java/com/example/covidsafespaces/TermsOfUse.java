a856129591fb38be0dc135be74f9b5628ed88661
Carlos <carlos.torra1998@gmail.com>
2020-10-29 10:35:26 +0100

    Term

--- App/app/src/main/java/com/example/covidsafespaces/TermsOfUse.java
+++ App/app/src/main/java/com/example/covidsafespaces/TermsOfUse.java
@@ -40,6 +40,8 @@ public class TermsOfUse extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(),reply,Toast.LENGTH_SHORT).show();
                 }
 
+
+
             }
         });
 
