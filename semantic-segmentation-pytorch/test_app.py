e02de248bde12911d8a590ffb6437b17524240a3
Jorge <pueyo99@gmail.com>
2020-11-19 12:19:34 +0100

    Testing

--- semantic-segmentation-pytorch/test_app.py
+++ semantic-segmentation-pytorch/test_app.py
@@ -94,9 +94,10 @@ def people_masks():
 
 	num_people = json.loads(open("object_counter/json_results/"+filename+".json").read())
 	masks_found = json.loads(open("mask_detection/json_results/"+filename+".json").read())
+	num_people.update(masks_found)
 	print(num_people, masks_found, sep='\n')
 
-	return jsonify(num_people.update(masks_found))
+	return jsonify(num_people)
      
 
 if __name__ == '__main__':
